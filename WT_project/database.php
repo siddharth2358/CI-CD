d8fc81e8660e0e6d15ac372ee8fb6c45144972b4
Siddharth <ysiddharthareddy09@gmail.com>
2025-11-05 10:22:10 +0530

    Fixed S3 upload syntax

--- WT_project/database.php
+++ WT_project/database.php
@@ -1,12 +1,31 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
+$hostName   = "localhost";
+$dbUser     = "root";
 $dbPassword = "";
-$dbName = "login_register";
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+$dbName     = "login_register";
+
+// connect to MySQL server first (no database)
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword);
 if (!$conn) {
-    die("Something went wrong;");
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// create database if it doesn't exist
+$createDbSql = "CREATE DATABASE IF NOT EXISTS `$dbName` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
+if (!mysqli_query($conn, $createDbSql)) {
+    die("Failed to create database: " . mysqli_error($conn));
+}
+
+// select the database
+mysqli_select_db($conn, $dbName);
+
+// set charset
+mysqli_set_charset($conn, 'utf8mb4');
+
+// optional: check final connection
+if (!$conn) {
+    die("Something went wrong: " . mysqli_connect_error());
 }
 
 ?>
